bce8a052f17caed5e22c98f1f5098056768b8e38
Sudeep G <sudeep@coffeebeans.io>
2020-01-15 12:54:58 +0530

    [Sourav/Sudeep] Refactor code

--- src/measurement.js
+++ src/measurement.js
@@ -6,27 +6,24 @@ export default class Measurement {
         this.unit = unit;
     }
     equals(measurement) {
-        if (this.areUnitsEqual(measurement)) {
-            const incomingMeasurementObjInCommonUnit = this.convertToCommonUnit(measurement);
-            return incomingMeasurementObjInCommonUnit.value === this.value;
-        }
-        throw new Error('Invalid Unit types!');
+        const incomingMeasurementObjInCommonUnit = this.convertToCommonUnit(measurement);
+        return incomingMeasurementObjInCommonUnit.value === this.value;
     }
     add(measurement) {
-        if (this.areUnitsEqual(measurement)) {
-            const incomingMeasurementObjInCommonUnit = this.convertToCommonUnit(measurement);
-            const resultantValueInCommonUnit = incomingMeasurementObjInCommonUnit.value + this.value;
-            return new Measurement(resultantValueInCommonUnit, this.unit);
-        }
-        throw new Error('Invalid Unit types!');
+        const incomingMeasurementObjInCommonUnit = this.convertToCommonUnit(measurement);
+        const resultantValueInCommonUnit = incomingMeasurementObjInCommonUnit.value + this.value;
+        return new Measurement(resultantValueInCommonUnit, this.unit);
     }
     areUnitsEqual(measurement) {
         return measurement instanceof Measurement && measurement.unit.type === this.unit.type;
     }
     convertToCommonUnit(measurement) {
-        return new Measurement(
-            ((measurement.value * measurement.unit.toUnitFactor) / this.unit.toUnitFactor),
-            this.unit
-        );
+        if (this.areUnitsEqual(measurement)) {
+            return new Measurement(
+                ((measurement.value * measurement.unit.toUnitFactor) / this.unit.toUnitFactor),
+                this.unit
+            );
+        }
+        throw new Error('Invalid Unit types!');
     }
 }
